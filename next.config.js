91f2f4ca69a8503ba31f74afd21cd0219223d267
shawn d'souza <shawnkdsouza@outlook.com>
2022-10-05 19:41:24 -0400

    test CORS fix

--- next.config.js
+++ next.config.js
@@ -7,6 +7,20 @@ module.exports = withStoreConfig({
   images: {
     domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/store/:products*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 })
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
