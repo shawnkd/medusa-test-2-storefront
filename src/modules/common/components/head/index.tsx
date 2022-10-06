1dd61c545fefe7b98e12d226d089e42138ddb59e
shawn d'souza <shawnkdsouza@outlook.com>
2022-10-05 23:50:26 -0400

    mobile preview

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -4,10 +4,10 @@ import React from "react"
 type HeadProps = {
   title1?: string
   description1?: string | null
-  image1?: string | null
+  image?: string | null
 }
 
-const Head: React.FC<HeadProps> = ({ title1, description1, image1 }) => {
+const Head: React.FC<HeadProps> = ({ title1, description1, image }) => {
   return (
     <NextHead>
       <title>OS FRAGILES</title>
@@ -17,7 +17,7 @@ const Head: React.FC<HeadProps> = ({ title1, description1, image1 }) => {
       <meta itemProp="og:description" content="by cammi casino"/>
       <meta itemProp="og:image" content="/osf-logo.jpeg" />
       {/* {description1 && <meta itemProp="description" content="by cammi casino" />} */}
-      {image1 && <meta itemProp="image" content="/osf-logo.jpeg" />}
+      {image && <meta itemProp="image" content="/osf-logo.jpeg" />}
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   )
