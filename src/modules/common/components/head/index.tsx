89fb51f4b231063d64c42ff197eb3da7545def81
shawn d'souza <shawnkdsouza@outlook.com>
2022-10-05 23:56:23 -0400

    mobile preview

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -12,12 +12,12 @@ const Head: React.FC<HeadProps> = ({ title, description, image }) => {
     <NextHead>
       <title>OS FRAGILES</title>
       <meta name="description" content="by cammi casino" />
-      {/* <meta itemProp="twitter:card" content="by cammi casino" /> */}
+      <meta itemProp="twitter:card" content="by cammi casino" />
       <meta itemProp="og:title" content="OS FRAGILES" />
       <meta itemProp="og:description" content="by cammi casino"/>
-      <meta itemProp="og:image" content="/osf-logo.jpeg" />
+      <meta property="og:image" content="/osf-logo.jpeg" />
       {description && <meta itemProp="description" content="by cammi casino" />}
-      {image && <meta itemProp="image" content="/osf-logo.jpeg" />}
+      {image && <meta itemProp="image" content={image} />}
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   )
