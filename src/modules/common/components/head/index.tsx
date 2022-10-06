a19baff58a715a9af8e3593e78092c6084086a8c
shawn d'souza <shawnkdsouza@outlook.com>
2022-10-05 23:38:33 -0400

    mobile preview

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -2,20 +2,22 @@ import NextHead from "next/head"
 import React from "react"
 
 type HeadProps = {
-  title?: string
-  description?: string | null
-  image?: string | null
+  title1?: string
+  description1?: string | null
+  image1?: string | null
 }
 
-const Head: React.FC<HeadProps> = ({ title, description, image }) => {
+const Head: React.FC<HeadProps> = ({ title1, description1, image1 }) => {
   return (
     <NextHead>
       <title>OS FRAGILES</title>
       <meta name="description" content="by cammi casino" />
       <meta itemProp="twitter:card" content="by cammi casino" />
+      <meta itemProp="og:title" content="OS FRAGILES" />
+      <meta itemProp="og:description" content="by cammi casino"/>
       <meta itemProp="og:image" content="/osf-logo.jpeg" />
-      {description && <meta itemProp="description" content="by cammi casino" />}
-      {image && <meta itemProp="image" content="/osf-logo.jpeg" />}
+      {/* {description1 && <meta itemProp="description" content="by cammi casino" />} */}
+      {/* {image1 && <meta itemProp="image" content="/osf-logo.jpeg" />} */}
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   )
