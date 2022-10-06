ed027dde5cb095fc6e8509203d7ed0fb3464327d
shawn d'souza <shawnkdsouza@outlook.com>
2022-10-05 23:10:32 -0400

    mobile preview

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -11,12 +11,13 @@ const Head: React.FC<HeadProps> = ({ title, description, image }) => {
   return (
     <NextHead>
       <title>OS FRAGILES</title>
-      <meta itemProp="name" content={title} />
+      <meta name="description" content="by cammi casino" />
+      <link rel="icon" href="/favicon.ico" />
       <meta name="twitter:card" content="by cammi casino" />
       <meta property="og:image" content="/osf-logo.jpeg" />
       {/* {description && <meta itemProp="description" content={description} />}
       {image && <meta itemProp="image" content={image} />} */}
-      <link rel="icon" href="/favicon.ico" />
+      
     </NextHead>
   )
 }
