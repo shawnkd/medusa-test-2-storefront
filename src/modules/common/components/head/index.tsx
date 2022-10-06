b5d5ed7cd3ef32bbe4b0de42ef8c901dde3a8e78
shawn d'souza <shawnkdsouza@outlook.com>
2022-10-06 00:00:00 -0400

    mobile preview

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -17,7 +17,7 @@ const Head: React.FC<HeadProps> = ({ title, description, image }) => {
       <meta itemProp="og:description" content="by cammi casino"/>
       <meta property="og:image" content="/osf-logo.jpeg" />
       {description && <meta itemProp="description" content="by cammi casino" />}
-      {image && <meta itemProp="image" content={image} />}
+      {/* {image && <meta itemProp="image" content={image} />} */}
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   )
